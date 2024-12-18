1a7495dce6b18ba8ca356a49ab7fdbf432845b34
fitse01 <fitsumtafese01@gmail.com>
2024-12-18 15:36:06 +0300

    update

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -15,8 +15,8 @@ const upload = multer({ dest: 'uploads/' });
 // Routes
 router.get('/get-all-product', getAllProducts);
 router.get('/get-product-id/:id', getProductById);
-router.post('/create-product', upload.array('images', 5), createProduct); // Accept up to 5 images
-router.put('/update-product-id/:id', upload.array('images', 5), updateProduct); // Accept up to 5 images
+router.post('/create-product', upload.array('images', 10), createProduct); // Accept up to 5 images
+router.put('/update-product-id/:id', upload.array('images', 10), updateProduct); // Accept up to 5 images
 router.delete('/delete-product-id/:id', deleteProduct);
 
 module.exports = router;
